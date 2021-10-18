37537cd7ca624f00dced21de8f63572bd836200b
Les Matheson <lmatheson4@bloomberg.net>
2021-10-18 13:43:34 -0400

    wip

--- hyrule-jobs/src/App.vue
+++ hyrule-jobs/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-<div class="app">Hello</div>
+    <div class="app">
+        <p>{{name}}</p>
+        <button @click="changeName('zelda')">Change Name</button>
+    </div>
 </template>
 
 <script lang="ts">
@@ -8,7 +11,18 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'App',
   components: {
-  }
+  },
+  data() {
+      return {
+          name: 'Foo hybrid'
+          };
+      },
+  methods: {
+      changeName(name:string) {
+          this.name=name;
+          }
+      }
+
 });
 </script>
 
